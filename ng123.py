5904b24763163730a4afdc1b28137097056308e8
Nglicloud <81097125+Nglicloud@users.noreply.github.com>
2025-02-19 19:53:26 +0530

    Update ng123.py

--- ng123.py
+++ ng123.py
@@ -1,3 +1,11 @@
 print("Hello All")
 print("it is new code")
 print("it is new project")
+
+print("Hello All")
+print("it is new code")
+print("it is new project")
+
+print("Hello All")
+print("it is new code")
+print("it is new project")
